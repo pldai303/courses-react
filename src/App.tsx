691497d1b6ef1e5bb5a2a92a7f9bc6322e884cab
Andrey Ivanov <paladin.ai.303@gmail.com>
2022-01-27 14:11:37 +0200

    sprint-7

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 
-import { FC, ReactNode, useEffect, useRef, useState } from 'react';
+import { FC, ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import NavigatorResponsive from './components/common/navigator-responsive';
 import { Subscription} from 'rxjs';
-import { PATH_COURSES, PATH_LOGIN, routes } from './config/routes-config';
+import { developmentRoutes, PATH_COURSES, PATH_LOGIN, routes } from './config/routes-config';
 import { authService, college } from './config/service-config';
 import Course from './models/course';
 import CoursesStore from './models/courses-store-type';
 import CoursesContext, { initialCourses } from './store/context';
 import { RouteType } from './models/common/route-type';
 import { UserData } from './models/common/user-data';
-import { Typography } from '@mui/material';
+import { Alert, Stack, Typography } from '@mui/material';
+import process, { allowedNodeEnvironmentFlags } from 'process';
 
 
 function getRelevantRoutes(userData: UserData): RouteType[] {
-  return routes.filter(r => (!!userData.username && r.authenticated)
+  let resRoutes = routes;
+  if (process.env.NODE_ENV === 'development') {
+    resRoutes = resRoutes.concat(developmentRoutes);
+  }
+  return resRoutes.filter(r => (!!userData.username && r.authenticated)
    || (userData.isAdmin && r.adminOnly) || (!userData.username && !r.authenticated && !r.adminOnly))
 }
 const App: FC = () => {
 
   const [storeValueState, setStoreValue] = useState<CoursesStore>(initialCourses);
-  
-
-  function addCourse(course: Course) {
-    college.addCourse(course).then();
-  }
-
-  function removeCourse(id: number) {
-    college.removeCourse(id).then();
-  }
  
   const [relevantRoutes, setRelevantRoutes] = useState<RouteType[]>(routes);
-  function getRoutes(): ReactNode[] {
-    return relevantRoutes.map(r => <Route key={r.path} path={r.path} element={r.element} />)
+  function getRoutes(): ReactNode[] {    
+    return relevantRoutes.map((r: RouteType) => <Route key={r.path} path={r.path} element={r.element} />)
   }
   useEffect (()=>{
     setRelevantRoutes(getRelevantRoutes((storeValueState.userData)));
@@ -51,21 +47,23 @@ const App: FC = () => {
     }
     
     const subscriptionUserData = getUserData();
-    
-    
-    return () => subscriptionUserData.unsubscribe();
+      return () => subscriptionUserData.unsubscribe();
   }, [])
+
   useEffect(() => {
     function getData(): Subscription {
       return college.getAllCourses().subscribe({
         next(arr) {
           storeValueState.list = arr;
+          
           setStoreValue({ ...storeValueState })
+        },
+        error(err) {
+        
+         setTimeout(getData, 2000); 
         }
       })
-    }
-    storeValueState.add = addCourse;
-    storeValueState.remove = removeCourse;
+    }  
     const subscription = getData();
     return () => subscription.unsubscribe();
   }, [])

5e2792f5bb39544cd0997173b9159cd96469b12b
Andrey Ivanov <paladin.ai.303@gmail.com>
2022-02-01 10:18:36 +0200

    sprint-8-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 
-import { FC, ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, ReactNode,  useEffect,  useState } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import NavigatorResponsive from './components/common/navigator-responsive';
 import { Subscription} from 'rxjs';
-import { developmentRoutes, PATH_COURSES, PATH_LOGIN, routes } from './config/routes-config';
+import { developmentRoutes,  routes } from './config/routes-config';
 import { authService, college } from './config/service-config';
-import Course from './models/course';
 import CoursesStore from './models/courses-store-type';
 import CoursesContext, { initialCourses } from './store/context';
 import { RouteType } from './models/common/route-type';
 import { UserData } from './models/common/user-data';
-import { Alert, Stack, Typography } from '@mui/material';
-import process, { allowedNodeEnvironmentFlags } from 'process';
+import { Alert } from '@mui/material';
+import process from 'process';
+import { AUTH_TOKEN } from './services/courses-service-rest';
+
+
+ 
 
 
 function getRelevantRoutes(userData: UserData): RouteType[] {
@@ -25,61 +28,77 @@ function getRelevantRoutes(userData: UserData): RouteType[] {
 const App: FC = () => {
 
   const [storeValueState, setStoreValue] = useState<CoursesStore>(initialCourses);
+  const [flErrorServer, setFlErrorServer] = useState<boolean>(false);
+
+  
  
   const [relevantRoutes, setRelevantRoutes] = useState<RouteType[]>(routes);
-  function getRoutes(): ReactNode[] {    
+  function getRoutes(): ReactNode[] {
     return relevantRoutes.map((r: RouteType) => <Route key={r.path} path={r.path} element={r.element} />)
   }
   useEffect (()=>{
     setRelevantRoutes(getRelevantRoutes((storeValueState.userData)));
-
   }, [storeValueState.userData]) 
+  
   useEffect(() => {
-    
-      function getUserData(): Subscription {
+       function getUserData(): Subscription {
         return authService.getUserData().subscribe({
           next(ud) {
             storeValueState.userData = ud;
             setStoreValue({ ...storeValueState })
           }
         })
-  
     }
     
     const subscriptionUserData = getUserData();
-      return () => subscriptionUserData.unsubscribe();
+    
+    
+    return () => subscriptionUserData.unsubscribe();
   }, [])
-
   useEffect(() => {
+    let subscription = getData();
     function getData(): Subscription {
       return college.getAllCourses().subscribe({
+       
         next(arr) {
+          setFlErrorServer(false);
           storeValueState.list = arr;
-          
           setStoreValue({ ...storeValueState })
         },
         error(err) {
-        
-         setTimeout(getData, 2000); 
+          if (err["code"] === 401) {
+            localStorage.setItem(AUTH_TOKEN, "");  
+          } else {
+          setFlErrorServer(true);
+           setTimeout(() => {subscription = getData()}, 2000);
+          }
         }
+
       })
-    }  
-    const subscription = getData();
+    }
     return () => subscription.unsubscribe();
   }, [])
-  return <CoursesContext.Provider value={storeValueState}>
 
+ 
+
+
+
+
+
+  return <CoursesContext.Provider value={storeValueState}>
+{flErrorServer ? <Alert severity='error'>Server is unavailable</Alert> : 
     <BrowserRouter>
       <NavigatorResponsive items={relevantRoutes} />
+     
+      
       <Routes>
-        {getRoutes()}
-        
+      {getRoutes()}
       <Route path={'*'} element={<Navigate to={relevantRoutes[0].path}/>}/>
       </Routes> 
      
 
     </BrowserRouter>
-
+}
   </CoursesContext.Provider>
 }
 
